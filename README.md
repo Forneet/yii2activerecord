f1f38ca137f1f91fc09ce76c116f9109de595f35
DESKTOP-CJ3VTRA\fornit-i7 <forneet@gmail.com>
2018-06-12 15:19:47 +0300

    install yii + test page

--- README.md
+++
@@ -1,2 +0,0 @@
-# yii2activerecord
-activerecord test
---
+++ basic/models/TestForm.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrey
+ * Date: 26.03.2016
+ * Time: 9:32
+ */
+
+namespace app\models;
+use yii\base\Model;
+
+
+class TestForm extends Model{
+
+    public $name;
+    public $email;
+    public $text;
+
+    public function attributeLabels(){
+        return [
+            'name' => 'Имя',
+            'email' => 'E-mail',
+            'text' => 'Текст сообщения',
+        ];
+    }
+
+    public function rules(){
+        return [
+            [ ['name', 'email'], 'required' ],
+            [ 'email', 'email' ],
+//            [ 'name', 'string', 'min' => 2, 'tooShort' => 'Мало' ],
+//            [ 'name', 'string', 'max'=> 5, 'tooLong' => 'Много' ]
+            [ 'name', 'string', 'length' => [2,5] ],
+            [ 'name', 'myRule' ],
+            [ 'text', 'trim' ],
+        ];
+    }
+
+    public function myRule($attr){
+        if( !in_array($this->$attr, ['hello', 'world']) ){
+            $this->addError($attr, 'Wrong!');
+        }
+    }
+
+} 
